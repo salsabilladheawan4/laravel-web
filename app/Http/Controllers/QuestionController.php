07bdf90279a9164ed028fb7564a4da7b13ea0e9d
salsabilla <salsabilla24ti@mahasiswa.pcr.ac.id>
2025-10-11 10:24:36 +0700

    salsabillaP4

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -28,7 +28,7 @@ class QuestionController extends Controller
      */
     public function store(Request $request)
     {
-        dd($request->all()) 
+        dd($request->all()) ;
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\QuestionControllerController;
+use App\Http\Controllers\QuestionController;
 
 Route::get('/home', [HomeController::class, 'index']);
 Route::post('question/store', [QuestionController::class, 'store'])
